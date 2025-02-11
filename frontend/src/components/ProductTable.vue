119204c58f629ff9217e3306ee47e0c5d95aa27b
Kyle Ferran <kyle@porch.software>
2025-02-11 13:54:45 -0700

    training sidebar

--- frontend/src/components/ProductTable.vue
+++ frontend/src/components/ProductTable.vue
@@ -8,8 +8,9 @@ const props = defineProps<{
 </script>
 
 <template>
-    <section class="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5">
+    <section class="p-3 sm:p-5">
         <div class="mx-auto max-w-screen-xl px-4 lg:px-12">
+            <h2 class="inline-block mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">Products Your Team Can Sell</h2>
             <!-- Start coding here -->
             <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
                 <div
