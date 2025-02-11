4a68c3c20b1b88855c29424eb1c6738f75f779f2
Alex Smith <asmith@porch.software>
2025-02-11 13:15:21 -0700

    ui changes

---
+++ frontend/src/components/TrainingScoreCard.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import BaseButton from '@/components/BaseButton.vue';
+import Card from '@/components/Card.vue';
+import { computed } from 'vue';
+
+const trainings = computed(() => [
+	{
+		label: 'Product Training',
+		completionPercentage: 50,
+		icon: 'share'
+	},
+	{
+		label: 'Dsitribution Group Training',
+		completionPercentage: 40,
+		icon: 'share'
+	},
+	{
+		label: 'Licensing Training',
+		completionPercentage: 100,
+		icon: 'share'
+	},
+	{
+		label: 'State Appointments',
+		completionPercentage: 20,
+		icon: 'share'
+	},
+	{
+		label: 'Continuing Education',
+		completionPercentage: 100,
+		icon: 'share'
+	}
+])
+</script>
+
+<template>
+<Card>
+	<div class="text-[#111928] dark:text-white text-[28px] font-extrabold leading-[35px]">My Training Scorecard</div>
+
+	<div
+		v-for="training in trainings"
+		class="mb-3 inline-flex w-full gap-1"
+	>
+		<div class="w-2/3">
+			<div class="flex justify-between mb-1">
+				<span class="text-base font-medium text-blue-700 dark:text-white">{{ training.label }}</span>
+				<span class="text-sm font-medium text-blue-700 dark:text-white">{{ training.completionPercentage }}%</span>
+			</div>
+			<div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+				<div class="bg-blue-600 h-2.5 rounded-full" :style="`width: ${training.completionPercentage}%`"></div>
+			</div>
+		</div>
+
+		<div class="w-1/3">
+			<BaseButton
+				:color="training.completionPercentage == 100 ? 'green' : 'default'"
+				
+			>
+				{{ training.completionPercentage == 100 ? 'Complete' : 'Train Now' }}
+			</BaseButton>
+		</div>
+
+	</div>
+</Card>
+</template>
--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -2,9 +2,13 @@
 import NavLayout from '@/layouts/NavLayout.vue';
 import TrainingActionCard from '@/components/TrainingActionCard.vue'
 import ProductTable from '@/components/ProductTable.vue';
+import Card from '@/components/Card.vue';
 import { useTraining } from '@/composables/useTraining';
 import { getProductTrainingCompletionPercentage } from '@/core/model';
 import { computed } from 'vue';
+import BaseButton from '@/components/BaseButton.vue';
+import Icon from '@/components/Icon.vue';
+import TrainingScoreCard from '@/components/TrainingScoreCard.vue';
 
 const { getProductTraining, producerTraining } = useTraining()
 
@@ -17,23 +21,63 @@ const anyProductsRequireTraining = computed(
 <template>
 <NavLayout>
     <div>
-        <div class="w-full py-24 bg-[#111928] flex-col justify-start items-center inline-flex">
-            <div v-if="anyProductsRequireTraining">
-                <div class="text-center text-white text-4xl font-extrabold leading-[45px]">Training Action Required</div>
+		<div class="w-full py-24 bg-[#111928] flex-col justify-start items-center inline-flex">
+			<div class="text-center text-white text-4xl font-extrabold leading-[45px]">Upcoming Required Trainings</div>
 
-                        <div class="text-center text-gray-400 text-xl font-normal leading-[30px] mb-5">The most pressing
-                            training modules that are applicable to your business and clients.</div>
+			<div class="grid grid-cols-3 gap-4 mt-5">
+				<Card>
+					<div class="text-[#111928] dark:text-white text-2xl font-bold leading-[30px]">
+						Continuing Education
+					</div>
 
-                <div class="grid grid-cols-2 gap-4">
-                    <TrainingActionCard
-                        v-for="product in producerTraining?.products"
-                        :product="product"
-                    />
-                </div>
-                
-            </div>
+					<div class="text-gray-500 dark:text-white text-base font-normal leading-normal mt-1">
+						Stay informed on the latest annuity products, regulations, and best practices—earn CE credits while sharpening your skills. 
+					</div>
+
+					<BaseButton class="inline-flex items-center mt-3">
+						<Icon name="share" class="mr-1"/>
+
+						Due 04 Oct 2026
+					</BaseButton>
+				</Card>
+
+				<Card>
+					<div class="text-[#111928] dark:text-white text-2xl font-bold leading-[30px]">
+						License Renewal
+					</div>
+
+					<div class="text-gray-500 dark:text-white text-base font-normal leading-normal mt-1">
+						Stay certified and continue serving your clients with confidence by completing your renewal requirements.
+					</div>
+
+					<BaseButton class="inline-flex items-center mt-3">
+						<Icon name="share" class="mr-1"/>
+
+						Due 04 Oct 2026
+					</BaseButton>
+				</Card>
+
+				<TrainingScoreCard />
+			</div>
+		</div>
+        <div
+			v-if="anyProductsRequireTraining"
+			class="w-full py-24 bg-[#111928] flex-col justify-start items-center inline-flex"
+		>
+			<div class="text-center text-white text-4xl font-extrabold leading-[45px]">Training Action Required</div>
+
+			<div class="text-center text-gray-400 text-xl font-normal leading-[30px] mb-5">
+				The most pressing training modules that are applicable to your business and clients.
+			</div>
+
+			<div class="grid grid-cols-2 gap-4">
+				<TrainingActionCard
+					v-for="product in producerTraining?.products"
+					:product="product"
+				/>
+			</div>
         </div>
-        <ProductTable :products="producerTraining.products" />
+        <ProductTable :products="producerTraining?.products ?? []" />
     </div>
 </NavLayout>
 
