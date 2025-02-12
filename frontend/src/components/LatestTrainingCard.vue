25a91c32cb1acd7cf0c2f6d6a0661838eef2efc4
Alex Smith <asmith@porch.software>
2025-02-12 11:17:43 -0700

    latest training

---
+++ frontend/src/components/LatestTrainingCard.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import BaseButton from '@/components/BaseButton.vue';
+import Card from '@/components/Card.vue';
+import Icon from '@/components/Icon.vue';
+import { type ICourse} from '@/core/model';
+import { computed } from 'vue';
+import { useOverlay } from '@/composables/useOverlay';
+import { useTraining } from '@/composables/useTraining';
+
+const training = useTraining()
+const overlay = useOverlay()
+
+const props = defineProps<{
+	course: ICourse
+}>()
+
+</script>
+
+<template>
+	<Card
+		:image="training.resolveTrainingImage(course)"
+	>
+		<div class="text-white text-lg font-bold leading-snug mt-5">{{ course.courseName }}</div>
+
+		<div class="text-gray-400 text-base font-normal leading-normal mt-1">Learn how it works, explore key riders, and understand how it fits into client strategies</div>
+
+		<div class="">
+			<BaseButton color="default" class="inline-flex items-center mt-5">
+				<Icon name="shield-check" class="mr-1"/>
+
+				Add to Training Queue
+			</BaseButton>
+		</div>
+		
+	</Card>
+</template>
